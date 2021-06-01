3a4c8b5a2a7c9fda634d21e9e2774e625a269ee2
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-01 11:46:52 +0530

    Add files via upload

---
+++ Paths/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Paths_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmpPath = @"C:\Users\Satya Maddila\source\repos\Paths Main\bin\Debug\netcoreapp3.1\file.txt";
+
+            string fileExtension = Path.GetExtension(tmpPath);
+            string filename = Path.GetFileName(tmpPath);
+            string filenameWithoutExtension = Path.GetFileNameWithoutExtension(tmpPath);
+            string rootPath = Path.GetPathRoot(tmpPath);
+            string directory = Path.GetDirectoryName(tmpPath);
+            string fullPath = Path.GetFullPath(tmpPath);
+
+            Console.WriteLine(fileExtension);
+            Console.WriteLine(filename);
+            Console.WriteLine(filenameWithoutExtension);
+            Console.WriteLine(rootPath);
+            Console.WriteLine(directory);
+            Console.WriteLine(fullPath);
+        }
+    }
+}
